4176333d73c27fc16c844afa7778ef58910af85c
alaurelut <axel.laurelut@cifacom.com>
2016-02-17 11:19:58 +0100

    added twig and map route and view

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Twig  = require('twig');
+var twig = Twig.twig;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var map = require('./routes/map');
 
 var app = express();
 
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/map', map);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
