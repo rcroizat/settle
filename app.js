eef689991d8a4ebba5acebc1afc209f6ade739c1
alaurelut <axel.laurelut@cifacom.com>
2016-02-22 12:02:01 +0100

    install mongodb

--- app.js
+++ app.js
@@ -71,3 +71,17 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://localhost:27017/';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+ 
+  db.close();
+});
