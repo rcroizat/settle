bcf832e5dbc7442127049dcfd7fcd9cc6eb23215
vqsto <romain.croizat@hotmail.fr>
2016-02-24 11:42:59 +0100

    home page if dc

--- public/javascripts/facebook/fb.js
+++ public/javascripts/facebook/fb.js
@@ -15,14 +15,9 @@ function statusChangeCallback(response) {
         user.getFacebookUser();
 
     } else if (response.status === 'not_authorized') {
-        // The person is logged into Facebook, but not your app.
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
+        window.location = "/";
     } else {
-        // The person is not logged into Facebook, so we're not sure if
-        // they are logged into this app or not.
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
+        window.location = "/";
     }
 }
 
