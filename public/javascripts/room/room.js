c6ad5d7a97a5f07c36b9f90ee685215a06742515
alaurelut <axel.laurelut@cifacom.com>
2016-02-26 16:14:21 +0100

    integration view chatroom

--- public/javascripts/room/room.js
+++ public/javascripts/room/room.js
@@ -15,20 +15,15 @@ socket.emit('userConnected', roomId, user);
 
 // Quand un nouveau client se connecte, on affiche l'information
 socket.on('userConnected', function(data) {
-
     var user = data.user;
-    var users = data.users;
-    $('#zone_users').empty();
-    for (var i = 0; i < users.length; i++) {
-        $('#zone_users').append('<li id=' + users[i].facebookId + '>' + users[i].name + ' </li>');
-    };
-    $('#zone_chat').prepend('<p><em>' + user.name + ' a rejoint le Chat !</em></p>');
+    $('#zone_chat').append('<p><em>' + user.name + ' a rejoint le Chat !</em></p>');
+    $(".roomContainer").animate({ scrollTop: $(".roomContainer").height() }, 1000);
 })
-
 
 // Quand on reçoit un message, on l'insère dans la page
 socket.on('message', function(data) {
-    insereMessage(data.date, data.user, data.message)
+    insereMessage(data.date, data.user, data.message);
+    $(".roomContainer").animate({ scrollTop: $(".roomContainer").height() }, 1000);
 });
 
 socket.on('isTyping', function(pseudo) {
@@ -59,5 +54,5 @@ $('#formulaire_chat').submit(function() {
 
 // Ajoute un message dans la page
 function insereMessage(date, user, message) {
-    $('#zone_chat').prepend('<div class="chat"><img class="profilPicture" src="' + user.profilPicture + '"><div class="user"><span class="username">' + user.name + '</span><span class="date">' + date + '</span><div class="message"> ' + message + '</div></div></div><div class="clear"></div>');
+    $('#zone_chat').append('<div class="chat"><img class="profilPicture" src="' + user.profilPicture + '"><div class="user"><span class="username">' + user.name + '</span><span class="date">' + date + '</span><div class="message"> ' + message + '</div></div></div><div class="clear"></div>');
 }
