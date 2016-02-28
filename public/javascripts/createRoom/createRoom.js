ee4680461d662aae7709b7320c60d89afe0e75ee
alaurelut <axel.laurelut@cifacom.com>
2016-02-28 14:25:56 +0100

    added custom checkbox on create room view

--- public/javascripts/createRoom/createRoom.js
+++ public/javascripts/createRoom/createRoom.js
@@ -9,9 +9,11 @@ var user = JSON.parse(localStorage.userData || '{}');
 
 var appendFriend = function(element, index, array) {
     var friend = document.getElementById('friendsList');
-    friend.innerHTML += '<li><label><input type="checkbox" name="friendId" value="' + element.id + '"><img src="http://graph.facebook.com/' + element.id + '/picture"/>' + element.name + '</label></li>';
+    // friend.innerHTML += '<li><input type="checkbox" name="friendId" class="customBox" value="' + element.id + '"><img src="http://graph.facebook.com/' + element.id + '/picture"/>' + element.name + '</label><label for="box"></label></li>';
+    friend.innerHTML += '<div class="friendInvit"><input type="checkbox" id="' + element.id + '" name="friendId" class="blaubarry"  value="' + element.id + '"/><label for="' + element.id + '"></label><img src="http://graph.facebook.com/' + element.id + '/picture"/></div>';
 };
 
 document.getElementById('userId').value = user.facebookId;
 document.getElementById('userName').value = user.name;
 user.facebookFriends.forEach(appendFriend);
+
